17b4cae1ebd03e45a6e665da9ea8780bd8b7064d
Gavryliv Sergiy <gavrylivsergiy1@gmail.com>
2024-04-19 11:07:25 +0300

    get_numbers_ticket

---
+++ get_numbers_ticket.py
@@ -0,0 +1,15 @@
+import random
+def get_numbers_ticket(min_num, max_num, quantity):
+    
+    if quantity > (max_num - min_num + 1):
+        raise
+    ValueError()
+    numbers = set()
+    while len(numbers) < quantity:
+
+        numbers.add(random.randint(min_num, max_num))
+    
+    return sorted(list(numbers))
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
